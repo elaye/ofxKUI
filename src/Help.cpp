c6d83e3add0997d6a99709a98d21d225ef5d9189
elaye <genard.elie@orange.fr>
2015-12-05 15:06:19 +0000

    Add camera help

--- src/Help.cpp
+++ src/Help.cpp
@@ -25,9 +25,39 @@ void Help::draw(){
   float x = 10;
   float y = 20;
   float l = 15;
+
   font.drawString("Help", x, y);
   y += l;
   font.drawString("---------------------", x, y);
   y += l;
   font.drawString("? Show/hide this help", x, y);
+  y += l;
+  font.drawString("[Esc] Return to normal mode", x, y);
+  y += 2 * l;
+
+  font.drawString("Camera mode [c]", x, y);
+  y += l;
+  font.drawString("---------------------", x, y);
+  y += 2 * l;
+
+  font.drawString("[s] Move down", x, y);
+  y += l;
+  font.drawString("[d] Move up", x, y);
+  y += l;
+  font.drawString("[f] Move right", x, y);
+  y += l;
+  font.drawString("[a] Move left", x, y);
+  y += l;
+  font.drawString("[w] Move forward", x, y);
+  y += l;
+  font.drawString("[x] Move backward", x, y);
+  y += 2 * l;
+
+  font.drawString("[k] Rotate down around horizontal axis", x, y);
+  y += l;
+  font.drawString("[l] Rotate up around horizontal axis", x, y);
+  y += l;
+  font.drawString("[j] Rotate left around vertical axis", x, y);
+  y += l;
+  font.drawString("[;] Rotate right around vertical axis", x, y);
 }
